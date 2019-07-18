d2dbc7a83c811fd6cb451f3389ecbdba2eced449
R4z1ell <creawshaka@gmail.com>
2019-07-18 19:19:02 +0200

    added redux and implemented userReducer and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -34,4 +35,9 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+// The 'state' that we pass as argument of this 'mapStateToProps' function is our 'root-reducer' pretty much
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
